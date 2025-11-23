2a77857e323800245757ef48ebbd3ab4c7dd69e6
Matheus-Spirit <matheusdiasmendespop@gmail.com>
2025-11-23 16:10:24 -0300

    adição teste spike

---
+++ atividade/tests/spike.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '30s', target: 10 },
+    { duration: '10s', target: 300 },
+    { duration: '1m', target: 300 },
+    { duration: '10s', target: 10 },
+  ],
+};
+
+export default function () {
+  const res = http.post('http://localhost:3000/checkout/simple');
+
+    check(res, {
+    'status é 200 ou 201': (r) => r.status === 200 || r.status === 201,
+    });
+
+  sleep(1);
+}
